85fb17c073db75d7c52f60a4acf68d7e91249b27
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2019-08-25 18:55:54 +0700

    add new file BaseActivity with blank content

---
+++ app/src/main/java/dyslexia/titi/frag27/base/BaseActivity.java
@@ -0,0 +1,7 @@
+package dyslexia.titi.frag27.base;
+// Created by Arif Ikhsanudin on 8/25/2019.
+
+import androidx.appcompat.app.AppCompatActivity;
+
+abstract class BaseActivity extends AppCompatActivity {
+}
