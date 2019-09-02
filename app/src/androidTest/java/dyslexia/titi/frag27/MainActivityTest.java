60168453319484907967427a26ff658b40d8c9dd
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2019-09-02 14:33:21 +0700

    add espresso test

---
+++ app/src/androidTest/java/dyslexia/titi/frag27/MainActivityTest.java
@@ -0,0 +1,26 @@
+package dyslexia.titi.frag27;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+// Created by Arif Ikhsanudin on 9/2/2019.
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void ensureIntentRegisterWork() {
+        onView(withId(R.id.btnRegister)).perform(click());
+    }
+}
