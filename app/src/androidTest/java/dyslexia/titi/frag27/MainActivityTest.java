580ac6ac4e865ae38edc5649725f3f8b909c09e8
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2019-09-02 09:36:52 +0700

    create main activity test

---
+++ app/src/androidTest/java/dyslexia/titi/frag27/MainActivityTest.java
@@ -0,0 +1,26 @@
+package dyslexia.titi.frag27;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+// Created by Arif Ikhsanudin on 9/2/2019.
+
+public class MainActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void masuk() {
+    }
+
+    @Test
+    public void register() {
+    }
+}
