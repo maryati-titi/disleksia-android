36417905cdc4e5b0371d6665098aba74353a91b8
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-10-22 21:55:20 +0700

    login

---
+++ app/src/androidTest/java/dyslexia/titi/app/MainActivityTest.java
@@ -0,0 +1,23 @@
+package dyslexia.titi.app;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void login() {
+    }
+
+    @Test
+    public void register() {
+    }
+}
