2c6a24ce8bdef7b51b433baa60058f32b8d7f745
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-10-23 10:16:25 +0700

    test go to login screen

--- app/src/androidTest/java/dyslexia/titi/app/MainActivityTest.java
+++ app/src/androidTest/java/dyslexia/titi/app/MainActivityTest.java
@@ -1,20 +1,36 @@
 package dyslexia.titi.app;
 
+import android.content.Intent;
+
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import dyslexia.app.ui.authentication.MainActivity;
+import dyslexia.app.ui.authentication.login.LoginActivity;
+
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
-    @Test
-    public void onCreate() {
-    }
+    @Rule
+    public IntentsTestRule<MainActivity> intentsTestRule =
+            new IntentsTestRule<>(MainActivity.class);
+
 
     @Test
-    public void login() {
+    public void goToLoginScreen() {
+        Intents.init();
+        intentsTestRule.launchActivity(new Intent());
+        intended(hasComponent(LoginActivity.class.getName()));
+        Intents.release();
     }
 
     @Test
